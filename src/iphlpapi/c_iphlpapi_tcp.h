d4eece3192674ee5ee25f5deabc66eb31659e805
985318924@qq.com <985318924@qq.com>
2025-10-02 07:26:37 +0800

    modify redefion

--- src/iphlpapi/c_iphlpapi_tcp.h
+++ src/iphlpapi/c_iphlpapi_tcp.h
@@ -1,23 +1,23 @@
 #include <windows.h>
 #include <iphlpapi.h>
 #include <stdio.h>
-
+#include <tcpmib.h>
 // 定义 MIB_TCPROW2 结构体
-typedef struct _MIB_TCPROW2 {
-	DWORD dwState;
-	DWORD dwLocalAddr;
-	DWORD dwLocalPort;
-	DWORD dwRemoteAddr;
-	DWORD dwRemotePort;
-	DWORD dwOwningPid;
-	DWORD dwOffloadState;
-} MIB_TCPROW2, *PMIB_TCPROW2;
+// typedef struct _MIB_TCPROW2 {
+// 	DWORD dwState;
+// 	DWORD dwLocalAddr;
+// 	DWORD dwLocalPort;
+// 	DWORD dwRemoteAddr;
+// 	DWORD dwRemotePort;
+// 	DWORD dwOwningPid;
+// 	DWORD dwOffloadState;
+// } MIB_TCPROW2, *PMIB_TCPROW2;
 
 // 定义 MIB_TCPTABLE2 结构体
-typedef struct _MIB_TCPTABLE2 {
-	DWORD dwNumEntries;
-	MIB_TCPROW2 table[ANY_SIZE];
-} MIB_TCPTABLE2, *PMIB_TCPTABLE2;
+// typedef struct _MIB_TCPTABLE2 {
+// 	DWORD dwNumEntries;
+// 	MIB_TCPROW2 table[ANY_SIZE];
+// } MIB_TCPTABLE2, *PMIB_TCPTABLE2;
 
 // 定义 GETEXTENDEDTABLE 函数指针类型
 typedef DWORD(WINAPI* GETEXTENDEDTABLE)(PVOID, PDWORD, BOOL, ULONG, TCP_TABLE_CLASS, ULONG);
